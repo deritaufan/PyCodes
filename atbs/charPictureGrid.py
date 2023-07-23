0f952871f3d4322956168f0756d14c7726b3a5ad
Deri Taufan <deritaufan@gmail.com>
2023-07-23 11:54:08 +0200

    added charPictureGrid.py

---
+++ atbs/charPictureGrid.py
@@ -0,0 +1,18 @@
+import copy
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+newGrid = copy.deepcopy(grid)
+for x in range(len(newGrid[0])):
+    for y in range(len(newGrid)):
+        print(newGrid[y][x], end='')
+    print()
+
